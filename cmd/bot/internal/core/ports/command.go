7ebf07c55a5969092de0c3603ab205660acfb451
クロナミ Abyss <joibelitz374@hotmail.com>
2026-03-12 02:23:13 +0300

    ✨ (bot): Add user entity to command handler interface

--- cmd/bot/internal/core/ports/command.go
+++ cmd/bot/internal/core/ports/command.go
@@ -4,8 +4,9 @@ import (
 	"context"
 
 	"github.com/thebeyond-net/control-plane/cmd/bot/internal/core/application/input"
+	"github.com/thebeyond-net/control-plane/internal/core/domain"
 )
 
 type CommandHandler interface {
-	Execute(ctx context.Context, msg input.Message) error
+	Execute(ctx context.Context, msg input.Message, user domain.User) error
 }
