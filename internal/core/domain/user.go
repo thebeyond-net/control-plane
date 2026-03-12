d9a061802025d7323d23b89f644de384e4559c39
クロナミ Abyss <joibelitz374@hotmail.com>
2026-03-12 12:42:28 +0300

    ✨ feat(domain): add user bandwidth with formatter

--- internal/core/domain/user.go
+++ internal/core/domain/user.go
@@ -1,6 +1,7 @@
 package domain
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -9,7 +10,15 @@ type User struct {
 	Identity
 	Subscription
 	Devices      int
+	Bandwidth    int
 	LanguageCode string
 	CurrencyCode string
 	CreatedAt    time.Time
+}
+
+func (user User) GetFormattedBandwidth() string {
+	if user.Bandwidth >= 1000 {
+		return "1 Gbps"
+	}
+	return fmt.Sprintf("%d Mbps", user.Bandwidth)
 }
